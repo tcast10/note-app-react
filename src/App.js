c91e25574ef009566e7323bb79483ceb7a742d6c
Teddy <tcastro@sandiego.edu>
2019-07-31 15:34:27 +0000

    Adding delete tag ability

--- src/App.js
+++ src/App.js
@@ -73,6 +73,12 @@ class App extends Component {
     .catch((err) => console.log(err.response.data) );
   }
 
+  deleteTag = (noteId, id) => {
+    axios.delete(urlFor(`/tags/${id}`))
+    .then((res) => this.getNote(noteId) )
+    .catch((err) => console.log(err.response.data) );
+  }
+
   render() {
     const { showNote, notes, note, newTag } = this.state;
 
@@ -87,6 +93,7 @@ class App extends Component {
             showTagForm={this.showTagForm}
             closeTagForm={this.closeTagForm}
             submitTag={this.submitTag}
+            deleteTag={this.deleteTag}
           /> 
           : 
           <List 
