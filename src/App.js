a93a9b7077b167962e9f210f7e8d811ac281fb0b
Teddy <tcastro@sandiego.edu>
2019-07-31 15:56:02 +0000

    Add error message, have it only stay for 2 seconds

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import List from './components/List';
 import Note from './components/Note';
 import axios from 'axios';
 import urlFor from './helpers/urlFor';
+import Flash from './components/Flash';
 
 
 class App extends Component {
@@ -15,7 +16,8 @@ class App extends Component {
       showNote: false,
       notes: [],
       note: {},
-      newTag: false
+      newTag: false,
+      error: ''
     };
   }
 
@@ -49,7 +51,14 @@ class App extends Component {
   submitNote = (data, id) => {
     this.performSubmissionRequest(data, id)
     .then((res) => this.setState({ showNote: false }) )
-    .catch((err) => console.log(err.response.data) );
+    .catch((err) => { 
+      const { errors } = err.response.data;
+      if (errors.content) {
+        this.setState({ error: "Missing Note Content!" });
+      } else if (errors.title) {
+        this.setState({ error: "Missing Note Title!" });
+      }
+    });
   }
 
   deleteNote = (id) => {
@@ -70,7 +79,12 @@ class App extends Component {
   submitTag = (data, noteId) => {
     axios.post(urlFor(`notes/${noteId}/tags`), data)
     .then((res) => this.getNote(noteId) )
-    .catch((err) => console.log(err.response.data) );
+    .catch((err) => {
+      const { errors } = err.response.data;
+      if (errors.name) {
+        this.setState({ error: "Missing Tag Name!"});
+      }
+    });
   }
 
   deleteTag = (noteId, id) => {
@@ -79,12 +93,17 @@ class App extends Component {
     .catch((err) => console.log(err.response.data) );
   }
 
+  resetError = () => {
+    this.setState({ error: '' });
+  }
+
   render() {
-    const { showNote, notes, note, newTag } = this.state;
+    const { showNote, notes, note, newTag, error } = this.state;
 
     return (
       <div className="App">
         <Nav toggleNote={this.toggleNote} showNote={showNote} />
+        {error && <Flash error={error} resetError={this.resetError} />}
         { showNote ? 
           <Note 
             note={note}
