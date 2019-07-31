2d7b425a28a638a3d1a1156bb43aae5f5a882e9b
Teddy <tcastro@sandiego.edu>
2019-07-31 14:51:16 +0000

    Console log the added tag and make tag form reappear

--- src/App.js
+++ src/App.js
@@ -63,6 +63,10 @@ class App extends Component {
     this.setState({ newTag: true });
   }
 
+  closeTagForm = () => {
+    this.setState({ newTag: false });
+  }
+
   render() {
     const { showNote, notes, note, newTag } = this.state;
 
@@ -73,8 +77,9 @@ class App extends Component {
           <Note 
             note={note}
             submitNote={this.submitNote}
-            showTagForm={this.showTagForm}
             newTag={newTag}
+            showTagForm={this.showTagForm}
+            closeTagForm={this.closeTagForm}
           /> 
           : 
           <List 
